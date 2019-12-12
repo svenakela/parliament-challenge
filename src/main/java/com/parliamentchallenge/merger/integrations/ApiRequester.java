5110d6e72e55f2256fa1b36fe1d420bac1a8e71e
Sven Wesley <sven.wesley@trustly.com>
2019-12-12 10:32:13 +0100

    Prepare for Code Lunch

--- src/main/java/com/parliamentchallenge/merger/integrations/ApiRequester.java
+++ src/main/java/com/parliamentchallenge/merger/integrations/ApiRequester.java
@@ -1,6 +1,5 @@
 package com.parliamentchallenge.merger.integrations;
 
-import java.util.StringJoiner;
 import org.springframework.web.reactive.function.client.WebClient;
 import com.parliamentchallenge.merger.model.PersonWrapper;
 import com.parliamentchallenge.merger.model.SpeechesWrapper;
@@ -12,23 +11,17 @@ public final class ApiRequester {
 
   private static final String MEMBER_URI = "/personlista/?&utformat=json&iid=";
 
-  // Causing an ugly warning with a stack trace at boot but only a warning
   private final WebClient webClient = WebClient.create("http://data.riksdagen.se");
 
   public Mono<SpeechesWrapper> getSpeeches() {
-    return webClient.get()
-        .uri(SPEECH_URI)
-        .retrieve()
-        .bodyToMono(SpeechesWrapper.class);
+    // TODO: SOLVE!
+    return Mono.empty();
   }
 
   public Mono<PersonWrapper> getMember(final String id) {
 
-    final String callUri = new StringJoiner("").add(MEMBER_URI).add(id).toString();
-    return webClient.get()
-        .uri(callUri)
-        .retrieve()
-        .bodyToMono(PersonWrapper.class);
+    // TODO: SOLVE!
+    return Mono.empty();
   }
 
 }
--- src/main/java/com/parliamentchallenge/merger/logic/DataProvider.java
+++ src/main/java/com/parliamentchallenge/merger/logic/DataProvider.java
@@ -2,13 +2,7 @@ package com.parliamentchallenge.merger.logic;
 
 import com.parliamentchallenge.merger.integrations.ApiRequester;
 import com.parliamentchallenge.merger.model.MergedSpeech;
-import com.parliamentchallenge.merger.model.PersonWrapper;
-import com.parliamentchallenge.merger.model.SpeechTemplate;
-import com.parliamentchallenge.merger.model.Speeches;
-import com.parliamentchallenge.merger.model.SpeechesWrapper;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Schedulers;
 
 public final class DataProvider {
 
@@ -20,26 +14,9 @@ public final class DataProvider {
 
   public Flux<MergedSpeech> getLatest() {
 
-    return requester
-        .getSpeeches()
-        .map(SpeechesWrapper::unwrap)
-        .map(Speeches::getSpeeches)
-        .flatMapMany(Flux::fromIterable)
-        .parallel()
-        .runOn(Schedulers.parallel())
-        .flatMap(s -> Flux.concat(mergeSpeech(s)))
-        .log()
-        .sequential();
-  }
+    // TODO: SOLVE!
+    return Flux.empty();
 
-  private Mono<PersonWrapper> getPersonWhoSpoke(final SpeechTemplate speech) {
-    return requester.getMember(speech.getPersonId());
-  }
-
-  private Mono<MergedSpeech> mergeSpeech(final SpeechTemplate speech) {
-
-    return getPersonWhoSpoke(speech)
-        .map(p -> new MergedSpeech(speech, p.getPersonList().getPerson()));
   }
 
 }
